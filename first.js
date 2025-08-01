4c1bde5f4ba60bd3f70641ee901f9e3a1b3cfa6a
Mehak Garg <gargmehak1908@gmail.com>
2025-08-01 08:06:20 +0530

    upi integrated

--- first.js
+++ first.js
@@ -8,3 +8,6 @@ const footer = "Footer added in our website";
 console.log(footer);
 const Payment = "Integrated the payement gateway";
 console.log(Payment);
+
+const upi = "Integrated the upi also";
+console.log(upi);
